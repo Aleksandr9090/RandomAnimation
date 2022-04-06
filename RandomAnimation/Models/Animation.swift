e463a3de05539a849e3898d9a111ca3c1fa722cb
shutkin.90 <shutkin.90@mail.ru>
2022-04-06 20:05:18 +0300

    finish

--- RandomAnimation/Models/Animation.swift
+++ RandomAnimation/Models/Animation.swift
@@ -7,11 +7,85 @@
 
 import Foundation
 
-
-struct Animation {
-    let preset: String
-    let curve: String
-    let force: Double
-    let duration: Double
-    let delay: Double
+struct Animation  {
+    let preset: Preset
+    let curve: Curve
+    let force: Float
+    let duration: Float
+    let delay: Float
+    
+    static func getRandomAnimation() -> Animation {
+        let randomAnimation = Animation(
+            preset: Preset.allCases.randomElement() ?? Preset.fadeIn ,
+            curve: Curve.allCases.randomElement() ?? Curve.easeIn ,
+            force: round(Float.random(in: 0.5...1.5) * 100) / 100.0,
+            duration: round(Float.random(in: 1...2) * 100) / 100.0,
+            delay: round(Float.random(in: 0...0.5) * 100) / 100.0
+        )
+        return randomAnimation
+    }
 }
+
+
+enum Preset: String, CaseIterable {
+    case slideLeft
+    case slideRight
+    case slideDown
+    case slideUp
+    case squeezeLeft
+    case squeezeRight
+    case squeezeDown
+    case squeezeUp
+    case fadeIn
+    case fadeOut
+    case fadeOutIn
+    case fadeInLeft
+    case fadeInRight
+    case fadeInDown
+    case fadeInUp
+    case zoomIn
+    case zoomOut
+    case fall
+    case shake
+    case pop
+    case flipX
+    case flipY
+    case morph
+    case squeeze
+    case flash
+    case wobble
+    case swing
+}
+
+enum Curve: String, CaseIterable {
+    case easeIn
+    case easeOut
+    case easeInOut
+    case linear
+    case spring
+    case easeInSine
+    case easeOutSine
+    case easeInOutSine
+    case easeInQuad
+    case easeOutQuad
+    case easeInOutQuad
+    case easeInCubic
+    case easeOutCubic
+    case easeInOutCubic
+    case easeInQuart
+    case easeOutQuart
+    case easeInOutQuart
+    case easeInQuint
+    case easeOutQuint
+    case easeInOutQuint
+    case easeInExpo
+    case easeOutExpo
+    case easeInOutExpo
+    case easeInCirc
+    case easeOutCirc
+    case easeInOutCirc
+    case easeInBack
+    case easeOutBack
+    case easeInOutBack
+}
+
--- RandomAnimation/ViewControllers/ViewController.swift
+++ RandomAnimation/ViewControllers/ViewController.swift
@@ -5,16 +5,47 @@
 //  Created by Aleksandr on 06.04.2022.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
     
+    @IBOutlet var springAnimationView: SpringView!
     
+    @IBOutlet var presentLabel: UILabel!
+    @IBOutlet var curveLabel: UILabel!
+    @IBOutlet var forceLabel: UILabel!
+    @IBOutlet var durationLabel: UILabel!
+    @IBOutlet var delayLabel: UILabel!
+    
+    var animation = Animation(preset: .fadeIn, curve: .easeIn, force: 1.5, duration: 2, delay: 0.3)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateView()
     }
 
+    @IBAction func runSpringAnimation(_ sender: SpringButton) {
+        
+        springAnimationView.animation = "\(animation.preset)"
+        springAnimationView.curve = "\(animation.curve)"
+        springAnimationView.force = CGFloat(animation.force)
+        springAnimationView.duration = CGFloat(animation.duration)
+        springAnimationView.delay = CGFloat(animation.delay)
+        springAnimationView.animate()
+        updateView()
 
+        animation = Animation.getRandomAnimation()
+        sender.setTitle("Run \(animation.preset)", for: .normal)
+    }
 }
 
+extension ViewController {
+    private func updateView() {
+        presentLabel.text = "present: \(animation.preset)"
+        curveLabel.text = "curve: \(animation.curve)"
+        forceLabel.text = "force: \(animation.force)"
+        durationLabel.text = "duration: \(animation.duration)"
+        delayLabel.text = "delay: \(animation.delay)"
+    }
+}
