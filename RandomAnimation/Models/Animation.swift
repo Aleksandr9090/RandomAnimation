f6b0381359f967c670958746b406da851b00241c
shutkin.90 <shutkin.90@mail.ru>
2022-04-06 14:07:23 +0300

    setup storyboard

---
+++ RandomAnimation/Models/Animation.swift
@@ -0,0 +1,17 @@
+//
+//  Animation.swift
+//  RandomAnimation
+//
+//  Created by Aleksandr on 06.04.2022.
+//
+
+import Foundation
+
+
+struct Animation {
+    let preset: String
+    let curve: String
+    let force: Double
+    let duration: Double
+    let delay: Double
+}
